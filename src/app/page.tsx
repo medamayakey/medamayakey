acb50bca5183cca9fd46d36b8928f1acedb80975
Rina <vvivid0en@gmail.com>
2024-04-14 23:22:36 -0700

    create a component button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { ChevronRight } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
+    <main className="p-2">
       <Link href="/testpage">shadcn/ui and Aceternity test</Link>
+
+      <div className="mb-2">
+        <Button>Button</Button>
+      </div>
+      <div className="mb-2">
+        <Button variant="destructive">destructive</Button>
+      </div>
+      <div className="mb-2">
+        <Button variant="outline">outline</Button>
+      </div>
+      <div className="mb-2">
+        <Button variant="secondary">secondary</Button>
+      </div>
+      <div className="mb-2">
+        <Button variant="defaultOutline" size="sm">
+          Button
+        </Button>
+      </div>
     </main>
-  )};
+  );
+}
