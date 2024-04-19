c99f0b8d7eabe0cf2313de8799dcfad540debfae
Rina <vvivid0en@gmail.com>
2024-04-18 18:38:18 -0700

    feat: display recipe item

--- src/actions/api/fetchRandomRecipesData.ts
+++ src/actions/api/fetchRandomRecipesData.ts
@@ -1,7 +1,8 @@
+"use server";
 export async function fetchRecipesData() {
   try {
     const response = await fetch(
-      `https://api.spoonacular.com/recipes/random?query=${query}?apiKey=${process.env.SPOONACULAR_API_KEY}&number=18`
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.SPOONACULAR_API_KEY}&number=18`
     );
     if (!response.ok) {
       throw new Error("Failed to fetch data");
--- src/app/search/RecipesItem.tsx
+++ src/app/search/RecipesItem.tsx
@@ -1,47 +1,46 @@
-// "use client";
-import Image from 'next/image';
-import Link from 'next/link';
-import { Button } from '@/components/ui/button';
-import { RecipeDetails } from '@/contexts/recipeContext';
-import { useEffect, useState } from 'react';
-import { fetchRecipesData } from '../../actions/api/fetchRandomRecipesData';
-import RecipeData from '@/types/recipe';
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import RecipeData from "@/types/recipe";
+import { useCallback, useEffect, useState } from "react";
+import { fetchRecipesData } from "@/actions/api/fetchRandomRecipesData";
+import { RecipesItemButton } from "./RecipesItemButton";
+import { Button } from "@/components/ui/button";
 
-interface RecipesItemProps {
-  recipeItem: RecipeDetails;
-}
+export default function RecipesItem() {
+  const [recipeData, setRecipeData] = useState<RecipeData[]>([]);
 
-export default function RecipesItem({ recipeItem }: RecipesItemProps) {
-  // const response = await fetchRecipesData();
-  // const recipeData = (await response.json()) as RecipeData;
+  const recipesItems = useCallback(async () => {
+    const items = await fetchRecipesData();
+    return items;
+  }, []);
+
+  useEffect(() => {
+    recipesItems().then((items) => {
+      console.log("items", items);
+      setRecipeData(items.recipes);
+    });
+  }, []);
 
   return (
-    <>
-      <li className="relative grid grid-cols-3 gap-4">
-        <div className="rounded-md w-full bg-white shadow-md">
-          <Link href={`/search/${recipeItem.id}`}>
-            <Image
-              src={recipeItem.image}
-              alt={recipeItem.title}
-              width={640}
-              height={100}
-              className="rounded-t-md"
-            />
+    <div className="grid grid-cols-2 md:grid-cols-3  gap-4">
+      {recipeData.map((recipe: RecipeData) => (
+        <div className="rounded-md w-full bg-white shadow-md" key={recipe.id}>
+          <Link href={`/search/${recipe.id}`}>
+            <Image src={recipe.image} alt={recipe.title} width={640} height={100} className="rounded-t-md" />
           </Link>
 
           <div className="p-4">
-            <h2 className="line-clamp-2">{recipeItem.title}</h2>
-            <div className=" flex gap-2 justify-between mt-4">
+            <h2 className="line-clamp-2">{recipe.title}</h2>
+            <div className="flex gap-2 justify-between mt-4">
               <Button asChild className="w-full" size="sm" variant="outline">
-                <Link href={`/search/${recipeItem.id}`}>More</Link>
+                <Link href={`/search/${recipe.id}`}>More</Link>
               </Button>
-              <Button className="w-full" size="sm">
-                Add
-              </Button>
+              <RecipesItemButton recipe={recipe} />
             </div>
           </div>
         </div>
-      </li>
-    </>
+      ))}
+    </div>
   );
 }
