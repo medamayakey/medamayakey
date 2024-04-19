86d51206c6e071d51890775d840e3e82b83a0d09
yuki-92c <jdpmcs@gmail.com>
2024-04-19 10:23:19 -0700

    feat: change url path

--- src/actions/api/fetchFilteredRecipesData.ts
+++ src/actions/api/fetchFilteredRecipesData.ts
@@ -3,32 +3,31 @@ export default async function fetchFilteredRecipesData(
   cuisine: string[],
   intolerance: string[],
   diet: string[]
-) 
+)
 {
-
-
-  let url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.SPOONACULAR_API_KEY}&number=18`;
+  let url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.SPOONACULAR_API_KEY}`;
 
   if (cuisine.length > 0) {
     url += `&cuisine=${cuisine}`;
   }
-  
+
   if (intolerance.length > 0) {
     url += `&intolerance=${intolerance}`;
   }
-  
+
   if (diet.length > 0) {
     url += `&diet=${diet}`;
   }
 
   console.log(url);
+  return url;
 
-  return fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Failed to fetch data");
-      }
-      return response.json();
-    })
-    .then((data) => data.results);
+  // return fetch(url)
+  //   .then((response) => {
+  //     if (!response.ok) {
+  //       throw new Error("Failed to fetch data");
+  //     }
+  //     return response.json();
+  //   })
+  //   .then((data) => data.results);
 }
