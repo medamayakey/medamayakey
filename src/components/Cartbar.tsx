bde76243dd6b84644da43540c4e7a1828fe883ce
Rina <vvivid0en@gmail.com>
2024-04-21 23:54:26 -0700

    feat: fix width and height

--- src/components/Cartbar.tsx
+++ src/components/Cartbar.tsx
@@ -35,7 +35,7 @@ export default function Cartbar() {
   };
 
   return (
-    <aside className="max-w-72 border-l">
+    <aside className="max-w-72 min-w-72 border-l">
       <div className="flex items-center p-4 border-b ">
         <ShoppingCart className="mr-2" />
         <h2>Item to buy</h2>
