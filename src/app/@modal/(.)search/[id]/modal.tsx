0986e8ee60959dfb7b7fd61b4a8ba3276bd07cfa
Kai <kkai.chien0813@gmail.com>
2024-04-26 18:47:49 -0700

    fix: resolve an error in modal.tsx

--- src/app/@modal/(.)search/[id]/modal.tsx
+++ src/app/@modal/(.)search/[id]/modal.tsx
@@ -1,33 +1,42 @@
-"use client";
+'use client';
 
-import { type ElementRef, useEffect, useRef } from "react";
-import { useRouter } from "next/navigation";
-import { createPortal } from "react-dom";
-import { X } from "lucide-react";
+import { X } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { type ElementRef, useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
 
 export function Modal({ children }: { children: React.ReactNode }) {
-  const router = useRouter();
-  const dialogRef = useRef<ElementRef<"dialog">>(null);
+	const router = useRouter();
+	const dialogRef = useRef<ElementRef<'dialog'>>(null);
 
-  useEffect(() => {
-    if (!dialogRef.current?.open) {
-      dialogRef.current?.showModal();
-    }
-  }, []);
+	useEffect(() => {
+		if (!dialogRef.current?.open) {
+			dialogRef.current?.showModal();
+		}
+	}, []);
 
-  function onDismiss() {
-    router.back();
-  }
+	function onDismiss() {
+		router.back();
+	}
 
-  return createPortal(
-    <div className="absolute bg-black flex z-50 shadow-lg">
-      <dialog ref={dialogRef} className="relative rounded-md w-8/12 p-9" onClose={onDismiss}>
-        {children}
-        <div className="absolute top-3 right-3 cursor-pointer">
-          <X onClick={onDismiss} />
-        </div>
-      </dialog>
-    </div>,
-    document.getElementById("modal-root")!
-  );
+	const modalRoot = document.getElementById('modal-root');
+	if (!modalRoot) {
+		throw new Error('Modal root element not found');
+	}
+
+	return createPortal(
+		<div className='absolute bg-black flex z-50 shadow-lg'>
+			<dialog
+				ref={dialogRef}
+				className='relative rounded-md w-8/12 p-9'
+				onClose={onDismiss}
+			>
+				{children}
+				<div className='absolute top-3 right-3 cursor-pointer'>
+					<X onClick={onDismiss} />
+				</div>
+			</dialog>
+		</div>,
+		modalRoot,
+	);
 }
